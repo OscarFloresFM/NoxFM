b380ccc1038698ece3d88de28f24194383cb050c
NoxFM <oscar99.ofm@gmail.com>
2025-10-09 17:02:48 -0600

    Agregar el archivo de leccion para print en python

---
+++ python/01_Primer_Print.py
@@ -0,0 +1,30 @@
+#Hola la primera leccion de python es un simple print
+
+print("Hola Mundo")
+
+#Que hace el codigo?
+#El codigo imprime en pantalla lo que esta entre parentesis y comillas puede ser desde un simple texto hasta una operacion matematica
+
+#Por ejemplo:
+
+print(3+5)
+
+#El resultado sera 8
+
+#O tambien puede imprimir variables
+a = 10
+b = 20
+print(a + b)
+#El resultado sera 30
+
+#Tambien puede imprimir texto y variables juntos
+print("El resultado de a + b es:", a + b)
+#El resultado sera "El resultado de a + b es: 30"
+
+#O incluso puede imprimir multiples lineas
+print("Hola\nMundo")
+#El resultado sera:
+
+#Hola
+#Mundo
+#El \n es un caracter especial que indica un salto de linea existe otros caracteres especiales que puedes investigar por tu cuenta
