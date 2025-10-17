62dacb98ca65cfc9ff03705eb349f8e24d02838c
NoxFM <oscar99.ofm@gmail.com>
2025-10-17 16:59:18 -0600

    Agregue el archivo Sumador

---
+++ python/03_Sumador.py
@@ -0,0 +1,9 @@
+#Muy bien ahora mostraremos la suma de dos numeros ingresados por el usuario
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
+#Con esto creamos el sumador basico que toma dos entradas del usuario, las convierte a enteros,
+#Puedes cambiar int() por float() si deseas trabajar con numeros decimales
+#Tambien puedes cambiar el operador + por otros operadores como - , * , / para realizar diferentes operaciones
+#Tambien juega con las entradas para ver como reacciona el programa a diferentes tipos de datos
